4757da394196df14413cb0765e754da99f164fff
Luisa <luisa.lacave@grad.iprj.uerj.br>
2025-06-21 16:47:18 -0300

    UPDATE: configurações do servidor.

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,18 +1,15 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { contractController } from './routes/contractController'
 import { estatisticController } from './routes/estatisticController'
 import { playerController } from './routes/playerController'
 import { teamController } from './routes/teamController'
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
-import cors from '@fastify/cors'
-
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-
-
-app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(cors, {
   origin: 'http://localhost:5173',
@@ -24,10 +21,10 @@ app.register(estatisticController)
 app.register(playerController)
 app.register(teamController)
 
-app.get('/', () => {
-    return 'Meu BD po..'
+app.get('/', async () => {
+  return 'Meu BD po..'
 })
 
-app.listen({port:3333}).then(() => {
-    console.log('HTTP server running')
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 Servidor HTTP rodando em http://localhost:3333')
 })
