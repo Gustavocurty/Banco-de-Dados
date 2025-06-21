838217fb032ffddabd33cb5b7937cd0f8ddc38a0
Luisa <luisa.lacave@grad.iprj.uerj.br>
2025-06-21 16:50:05 -0300

    UPDATE

--- FrontEnd/src/components/nav.jsx
+++ FrontEnd/src/components/nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <nav className="bg-blue-500 p-4 shadow-md fixed top-0 left-0 w-full">
+    <nav className="bg-blue-500 p-4 shadow-md fixed top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold"><a href="/">Gestão de Jogadores de Futebol</a></div>
+        <div className="text-white text-2xl font-bold">
+          <Link to="/">Gestão de Jogadores de Futebol</Link>
+        </div>
         <div className="flex space-x-10">
-          <a href="/jogadores" className="text-white hover:text-blue-200">Jogadores</a>
-          <a href="/times" className="text-white hover:text-blue-200">Times</a>
-          <a href="/estatisticas" className="text-white hover:text-blue-200">Estatísticas</a>
-          <a href="/contratos" className="text-white hover:text-blue-200">Contratos</a>
+          <Link to="/jogadores" className="text-white hover:text-blue-200">Jogadores</Link>
+          <Link to="/times" className="text-white hover:text-blue-200">Times</Link>
+          <Link to="/estatisticas" className="text-white hover:text-blue-200">Estatísticas</Link>
+          <Link to="/contratos" className="text-white hover:text-blue-200">Contratos</Link>
         </div>
       </div>
     </nav>
